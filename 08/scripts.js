6e54a051f73c64cdcfc02775830d104b121370ba
HaazeValentine <vgaaze12@gmail.com>
2022-02-02 13:53:37 +0200

    homework08

---
+++ 08/scripts.js
@@ -0,0 +1,24 @@
+function task1() {
+    let pascal = prompt("Введіть тиск в Паскалях: ");
+    let bar =  pascal * 0.000010;
+    let atm = pascal * 0.0000098692326671601;
+    let mmHg = pascal * 0.0075006375541921;
+    alert(`${pascal}` + "Па = " + `${bar}` +  " bar = " + `${atm}` + " atm = " + `${mmHg}` + " mmHg");
+   
+    console.log(`${pascal}` + "Па = " + `${bar}` +  " bar = " + `${atm}` + " atm = " + `${mmHg}` + " mmHg");
+}
+function task2() {
+    let a, b, percent;
+    a = +prompt("Перше число");
+    b = +prompt("Друге число");
+    percent = a % b;
+    if (percent == 0)
+    {
+        alert("Числа діляться без остачі," + " частка від ділення = " + a / b);
+        console.log("Числа діляться без остачі," + " частка від ділення = " + a / b);
+    }
+    else {
+        alert("Числа діляться з остачею = " + percent + ", " + " частка від ділення = " + a / b);
+        console.log("Числа діляться з остачею = " + percent + ", " + " частка від ділення = " + a / b);
+    }
+}
