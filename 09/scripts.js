f4a74c563e50882ebbcd6f0f90aed76bb5051278
HaazeValentine <vgaaze12@gmail.com>
2022-04-18 16:29:19 +0300

    homework

---
+++ 09/scripts.js
@@ -0,0 +1,8 @@
+document.write('Hello world');
+document.write('<br>');
+document.write('<em> Hello world </em> ');
+document.write('<br>');
+let str = 'Hello world';
+document.write('<em>' + str + '</em>');
+document.write('<br>');
+document.write(1 ,'<br>', 2, '<br>', 3, '<br>', 4, '<br>', 5);
